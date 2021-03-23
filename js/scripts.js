99ba32f59097510df69fe1e97968fc350bb7856a
Patrick Lee <patricklee1138@live.com>
2021-03-23 14:02:12 -0700

    WIP: add 'dark mode' switch, script not working
    Co-authored-by: Jonathan Stull <jonathan.d.stull@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,26 @@
 $(document).ready(function() {
+
+  // $(".round-slider").is(":unchecked") {
+  //   $("body").removeClass();
+  // });
+
   $("button#green").click(function() {
+    $("body").removeClass();
     $("body").addClass("green-background");
   });
 
   $("button#yellow").click(function() {
+    $("body").removeClass();
     $("body").addClass("yellow-background");
   });
 
   $("button#red").click(function() {
+    $("body").removeClass();
     $("body").addClass("red-background");
   });
 });
+
+if ($("input#dark").is(":checked") {
+  $("body").removeClass();
+  $("body").addClass("dark-mode");
+}));
